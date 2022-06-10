a23b0fbf44355863ee7fb1f3a98c20013488d143
vijayringale <104748152+vijayringale@users.noreply.github.com>
2022-06-10 19:41:22 +0530

    Update and rename Day_one_training.go to Factorial.go

---
+++ Day_one_Assignment/Factorial.go
@@ -0,0 +1,26 @@
+
+
+import "fmt"
+
+var factVal uint64 = 1
+
+var i int = 1
+var n int
+
+func factorial(n int) uint64 {
+	if n < 0 {
+		fmt.Print("Factorial of negative number doesn't exist.")
+	} else {
+		for i := 1; i <= n; i++ {
+			factVal *= uint64(i)
+		}
+
+	}
+	return factVal
+}
+
+func main() {
+	fmt.Print("Enter a positive integer between 0 - 50 : ")
+	fmt.Scan(&n)
+	fmt.Print("Factorial is: ", factorial(n))
+}
