77475e8726e0ee9f09de7a2b3bc3f6097391604f
vijayringale <104748152+vijayringale@users.noreply.github.com>
2022-06-10 19:35:16 +0530

    Update Function_Call_reverse_a_string_using.go

--- Day_one_Assignment/Function_Call_reverse_a_string_using.go
+++ Day_one_Assignment/Function_Call_reverse_a_string_using.go
@@ -1,1 +1,25 @@
 
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+*********
+Write and call a function to reverse a string using for loop.
+*********
+
+func Reverse(s string) (result string) {
+
+	for i:=len(s)-1; i>=0; i--{
+		result =result+string(s[i])
+	}
+	fmt.Println(result)
+
+	return
+  }
+
+func main(){
+	Reverse("tnairox")
+}
