bc5d4ff4ec6cfe97bdcc5eb38f01a38f9a3e54bc
vijayringale <104748152+vijayringale@users.noreply.github.com>
2022-06-10 19:38:26 +0530

    Update pattern_using_loops_and_branching.go

--- Day_one_Assignment/pattern_using_loops_and_branching.go
+++ Day_one_Assignment/pattern_using_loops_and_branching.go
@@ -1,1 +1,34 @@
 
+package main
+
+import (
+	"fmt"
+	"math"
+)
+//*********
+/*Print the following pattern using loops and branching:
+1
+2 6
+3 7 10
+4 8 11 13
+5 9 12 14 15.
+*/
+
+func main() {
+	var num = 5
+	lowerLeftCorner := num*(num-1)/2 + 1
+	lastInColumn := lowerLeftCorner
+	lastInRow := 1
+	for i, row := 1, 1; row <= num; i++ {
+		w := len(fmt.Sprint(lastInColumn))
+		if i < lastInRow {
+			fmt.Printf("%*d ", w, i)
+			lastInColumn++
+		} else {
+			fmt.Printf("%*d\n", w, i)
+			row++
+			lastInRow += row
+			lastInColumn = lowerLeftCorner
+		}
+	}
+}
