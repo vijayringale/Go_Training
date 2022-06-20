24ae0a6b5ddc731c7c315e0510527dcf8918f840
vijayingale <91070528+vijayingale@users.noreply.github.com>
2022-06-20 08:55:39 +0530

    updated Array Addition

--- Day_two_Assignment/Array_Addition.go
+++ Day_two_Assignment/Array_Addition.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Point struct {
 	a   int
@@ -9,15 +11,15 @@ type Point struct {
 	sum int
 }
 
-func runtine1(p Point) {
-	slice := p.arr[p.a:p.b]
+func runtine1(a int, b int, arr []int, sum int) {
+	slice := arr[a:b]
 
 	for _, items := range slice {
-		fmt.Print(items)
-		p.sum = p.sum + items
+		fmt.Print(items, " ")
+		sum = sum + items
 	}
 
-	fmt.Print("\n\n", p.sum, "\n")
+	fmt.Print("\n\n", sum, "\n")
 }
 
 func main() {
@@ -28,16 +30,11 @@ func main() {
 		array = append(array, i)
 	}
 	fmt.Println(array)
-	p := Point{0, 20, array, 0}
-	k := Point{20, 40, array, 0}
-	w := Point{40, 60, array, 0}
-	q := Point{60, 80, array, 0}
-	z := Point{80, 99, array, 0}
 
-	go runtine1(p)
-	runtine1(k)
-	go runtine1(w)
-	runtine1(q)
-	runtine1(z)
+	go runtine1(0, 20, array, 0)
+	runtine1(20, 40, array, 0)
+	go runtine1(40, 60, array, 0)
+	runtine1(60, 80, array, 0)
+	runtine1(80, 99, array, 0)
 
 }
