ba65be7aa070ab6062279942677388bde33605cf
vijayringale <104748152+vijayringale@users.noreply.github.com>
2022-07-06 09:00:44 +0530

    Last Update

--- Day_Three_Assignment/sync_Atomic.go
+++ Day_Three_Assignment/sync_Atomic.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -9,38 +10,31 @@ import (
   
 
 var waittime sync.WaitGroup
-  
 var atmvar int32
   
 func hike(S string) {
   
     for i := 1; i < 7; i++ {
   
-        
-        time.Sleep(time.Duration( time.Millisecond))
+        time.Sleep(time.Duration((rand.Intn(5))) * time.Millisecond)
   
-        
+    
         atomic.AddInt32(&atmvar, 1)
   
-        
         fmt.Println(S, i, "count ->", atmvar)
     }
   
-    
     waittime.Done()
 }
   
-
 func main() {
   
-    
     waittime.Add(2)
   
    
-    go hike("Pushpa: ")
-    go hike("Bahubali: ")
+    go movie("bahubali 1 : ")
+    go movie("bahubali 2: ")
   
-    
     waittime.Wait()
   
     fmt.Println("The value of last count is :", atmvar)
